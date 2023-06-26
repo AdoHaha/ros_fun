90699bb505630a768c8c403c73fcd79426abf9cd
Igor <igorzubrycki@gmail.com>
2023-06-26 22:41:50 +0200

    working Topic examples

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,2 +1,2 @@
 export DISPLAY=:1.0
-jupyter notebook --notebook-dir="/home/ubuntu/turtlebot3_ws/src/jupyter_notebooks" --ip 0.0.0.0
+jupyter notebook --notebook-dir="/home/ubuntu/turtlebot3_ws/src/jupyter_notebooks" --ip 0.0.0.0 --no-browser
